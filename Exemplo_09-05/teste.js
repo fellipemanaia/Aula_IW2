fd576bc9c0415501aee44b275a550aec903bb729
Fellipe Manaia <100931530+fellipemanaia@users.noreply.github.com>
2023-05-09 11:01:36 -0300

    Add files via upload

---
+++ Exemplo_09-05/teste.js
@@ -0,0 +1,33 @@
+//var nome = prompt("Digite seu nome: ")
+//console.log(nome)
+
+
+//alert("Bem vindo "+ nome)
+
+//var variavél global
+//let variavél local
+//const variavél constante(não se altera)
+
+/*
+var a = 4
+if (true){
+    var a =10
+    console.log(a)
+}
+console.log(a)
+*/
+
+//Calculando a soma de 2 números
+
+var n1 = parseInt(prompt("Digite n1: ")) 
+var n2 = parseInt(prompt("Digite n2: "))
+
+var soma = (n1+n2)
+
+//console.log("a soma é " + soma)
+//console.log(typeof n1)
+
+
+//Template String
+//console.log(`A soma é ${soma}`)
+document.write(`<h2>A soma é ${soma}</h2>`)
